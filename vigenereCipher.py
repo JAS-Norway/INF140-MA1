1e2f99088a3ea15d3e8b12e717a15774505cd943
JAS-Norway <work.jas.norway@gmail.com>
2024-09-06 19:31:01 +0200

    Complete.
    First iteration.

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,16 +1,43 @@
 
-def encrypt():
-    pass
+def encrypt(plaintext, keystream):
+    ciphertext = ""
+    plaintextLength = len(plaintext)
+    for x in range(plaintextLength):
+        newLetterInt = (ord(plaintext[x])-97) + (ord(keystream[x])-97)
+        newLetterInt = newLetterInt % 26
+        ciphertext += chr(newLetterInt+97)
+    return ciphertext
 
-def decrypt():
-    pass
+def decrypt(ciphertext, keystream):
+    plaintext = ""
+    ciphertextLength = len(ciphertext)
+    for x in range(ciphertextLength):
+        newLetterInt = (ord(ciphertext[x])-97) - (ord(keystream[x])-97)
+        if newLetterInt < 0:
+            newLetterInt += 26
+        newLetterInt = newLetterInt % 26
+        plaintext += chr(newLetterInt+97)
+    return plaintext
 
 
+inputText = input("What do you want to encrypt/decrypt? ").lower()
+key = input("What is the key? ").lower()
 encryptOrDecrypt = input("Write 'e' to encrypt or 'd' to decrypt: ")
 
+inputTextLength = len(inputText)
+keyLength = len(key)
+
+while True:
+    if(keyLength >= inputTextLength):
+        break
+    else:
+        key += key
+        keyLength = len(key)
+
 if(encryptOrDecrypt == "e"):
-    encrypt()
+    print(encrypt(inputText, key))
 elif(encryptOrDecrypt == "d"):
-    decrypt()
+    print(decrypt(inputText, key))
 else:
-    pass
+    print("Wrong input.")
+    exit()
