b97303b1c3c5fd9c41679d6ca5199d38bf86fec7
JAS-Norway <work.jas.norway@gmail.com>
2024-09-06 18:49:30 +0200

    Initial commit

---
+++ vigenereCipher.py
@@ -0,0 +1,16 @@
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
+
+
+encryptOrDecrypt = input("Write 'e' to encrypt or 'd' to decrypt: ")
+
+if(encryptOrDecrypt == "e"):
+    encrypt()
+elif(encryptOrDecrypt == "d"):
+    decrypt()
+else:
+    pass
